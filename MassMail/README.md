dfbf0cc7de8a6f85e113725ec79a2f89a60d3ec8
Dhritesh Bhagat <dhriteshbhagat@gmail.com>
2021-07-10 18:42:00 +0530

    updarte mass mail

--- MassMail/README.md
+++
@@ -1,8 +0,0 @@
-# Automated Mailer 
-
-Automate The Boring Email Job :
-
-Make sure that u turn on third party access or Less app secure option in manage google account
-change the excel file link an the html link for the email sending process rest leave it up to the bot.
-
-***Note :*** In the text file containing the message put {} in places where you want data from Excel to be inserted
--- MassMail/main.py
+++ MassMail/main.py
@@ -2,10 +2,15 @@ import smtplib
 from email.message import EmailMessage
 import pandas as pd
 import codecs
+excelink=input("Enter the excel link : ")
+df = pd.read_excel(open(excelink,'rb')) # can also index sheet by name or fetch all sheets
+dftemp=pd.ExcelFile(excelink)
+print(dftemp.sheet_names)
+print(df.columns)
+column=input("Enter the Column Name: ")
+mylist = df[column].tolist()
 
-df = pd.read_excel('/content/drive/MyDrive/Colab Notebooks/emails.xlsx') # can also index sheet by name or fetch all sheets
-mylist = df['Emails'].tolist()
-
+print(mylist)
 #taking the email as the input
 emailid=input("Enter the Email ID: ")
 emailpasswd=input("Enter the Email Password: ")
@@ -19,14 +24,15 @@ msg['from'] = emailid
 msg['to'] = mylist
 
 #enter the html here but make sure it is in one line
-path="/content/drive/MyDrive/Colab Notebooks/email.html" 
+path=input("Enter the path for the email html : ")
 file=codecs.open(path,"r")
 htmlcode=file.read()
 msg.add_alternative(htmlcode,subtype='html')
 
+print("Your messages will be sent now! If you want to stop please stop now")
+n=input("Press enter to confirm or hit CTRL+C")
 
 with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
-    smtp.login(emailid,emailpasswd)
-    smtp.send_message(msg)
-
-print("messgae sent successfully")
+  smtp.login(emailid,emailpasswd)
+  smtp.send_message(msg)
+print("message sent successfully")
